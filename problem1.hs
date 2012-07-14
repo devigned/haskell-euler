fb81b8191053d0423b896ba016bb71f07f9bd6b2
David Justice <david@devigned.com>
2012-07-14 00:37:42 -0400

    problem 1 completed

---
+++ problem1.hs
@@ -0,0 +1,13 @@
+
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+-- The sum of these multiples is 23.
+
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+multipleOf :: (Integer, Integer) -> Bool
+multipleOf (n, y) = n `mod` y == 0
+multipleOf3or5 (n) = multipleOf(n, 3) || multipleOf(n, 5)
+sumOfMultiples (n) = sum [i | i <- filter multipleOf3or5 [1..n]]
+
+main = do
+  putStrLn(show(sumOfMultiples(1000 - 1)))
